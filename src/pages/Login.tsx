d934bbb5cda2f9c2d3398212ddc0ddff5d8bfce9
FelipeFernandesr <felipefernandesr90@gmail.com>
2024-11-25 23:00:05 -0300

    Feat: Form component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { Box, Container } from "@mui/material";
 import Grid from '@mui/material/Grid';  
-import { BannerImage } from '@/components';
+import { BannerImage, FormComponent, Logo, StyledH1, StyledP } from '@/components';
+import { pxToRem } from "@/utils";
 
 function Login() {
 
@@ -10,7 +11,23 @@ function Login() {
         <Grid container>
           <Grid item xs={12} sm={6} sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}>
             <Container maxWidth="sm">
-              <h1>LOGIN</h1>
+              <Box sx={{ marginBottom: pxToRem(24) }}><Logo height={41} width={100}/></Box>
+              <Box sx={{ marginBottom: pxToRem(24) }}>
+                <StyledH1>Bem-vindo</StyledH1>
+                <StyledP>Digite sua senha e email para logar</StyledP>
+              </Box>
+              <FormComponent inputs={[
+                { type: 'email', placeholder: 'Email' },
+                { type: 'password', placeholder: 'Senha' }
+              ]} 
+              buttons={[
+                { className: 'primary', type: 'submit', children: 'Login' },
+              ]} 
+              message= {{
+                msg: 'Sucesso!!!',
+                type: 'success',
+              }} 
+              />
             </Container>
           </Grid>
           <Grid sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
