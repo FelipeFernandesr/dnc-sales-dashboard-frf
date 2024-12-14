cf7d704ffbf5d58b947b4b5123cc6a4b3fc8c9cf
FelipeFernandesr <felipefernandesr90@gmail.com>
2024-12-14 13:00:18 -0300

    Feat: Switch Theme Logic

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { GlobalStyle, lighTheme } from './styles/'
+import { AppThemeProvider } from './contexts/AppThemeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={lighTheme}>
+    <AppThemeProvider>
       <GlobalStyle />
       <App />
-    </ThemeProvider>
+    </AppThemeProvider>
     
   </StrictMode>,
 )
