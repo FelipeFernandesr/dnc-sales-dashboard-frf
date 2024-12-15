f79bf09a978a7c8eb6f13e6d13721bc1f5f7086f
FELIPE FERNANDES <38325369+FelipeFernandesr@users.noreply.github.com>
2024-12-15 11:04:17 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
-import { ThemeProvider } from 'styled-components'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { GlobalStyle, lighTheme } from './styles/'
+import { GlobalStyle } from './styles/'
 import { AppThemeProvider } from './contexts/AppThemeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
