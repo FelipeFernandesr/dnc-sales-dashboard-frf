3a462e2c92e0bd9530ed544bf26ccbb347c100d4
FelipeFernandesr <felipefernandesr90@gmail.com>
2025-01-15 23:07:12 -0300

    Remove todos os arquivos da branch main

--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './avatarsList'
-export * from './customChart'
-export * from './customTable'
-export * from './theme'
-export * from './typographies'
-export * from './formComponent'
--- src/utils/pxToRem.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Convert pixels to rem
- * @param pixels - pixels value to be converted
- * @returns The converted rem value
- */
-export function pxToRem(pixels: number): string {
-    return `${pixels / 16}rem`
-}
